5813c3fccfd6de07c8167a47b9db677c83ae4f5e
RK <riku.kuu0421@gmail.com>
2023-06-30 23:43:38 +0900

    [update]書いたメモをデバックに表示するwindowを作成

--- Assets/ExpansionTest.cs
+++ Assets/ExpansionTest.cs
@@ -3,11 +3,21 @@ using UnityEditor;
 
 public class ExpansionTest : EditorWindow
 {
+    private string _memo;
+
     [MenuItem("Test/ExpansionTest1", false, 1)]
     private static void MyMenuItem()
     {
-        ExpansionTest window = CreateInstance<ExpansionTest>();
+        GetWindow<ExpansionTest>();
+    }
 
-        window.ShowAuxWindow();
+    private void OnGUI()
+    {
+        GUILayout.Label("Memo");
+        _memo = EditorGUILayout.TextArea(_memo, GUILayout.Height(EditorGUIUtility.singleLineHeight * 5));
+        if(GUILayout.Button("Apply"))
+        {
+            Debug.Log(_memo);
+        }
     }
 }
