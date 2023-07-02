15464f495a423ce52a329cf108772c2fccf6ed99
RK <riku.kuu0421@gmail.com>
2023-07-02 23:51:42 +0900

    [update]複数のscriptテンプレートから選択できるようにした

---
+++ Assets/Editor/ScriptGeneratorWindow.cs
@@ -0,0 +1,98 @@
+using UnityEditor;
+using UnityEngine;
+
+public class ScriptGeneratorWindow : EditorWindow
+{
+    private string _monoBehaviourTemplate =
+@"using UnityEngine;
+
+public class #SCRIPTNAME# : MonoBehaviour
+{
+　　//日本語
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}";
+
+    private string _classTemplate =
+@"using UnityEngine;
+
+public class #SCRIPTNAME#
+{
+　　//日本語
+}";
+
+    private string _interfaceTemplate =
+@"using UnityEngine;
+
+public interface #SCRIPTNAME#
+{
+　　//日本語
+}";
+
+    private string _scriptName = "NewScript";
+
+    private SelectTemplates _selectTemplates = SelectTemplates.MonoBehaviour;
+
+
+    public enum SelectTemplates
+    {
+        MonoBehaviour,
+        Class,
+        Interface,
+    }
+
+    [MenuItem("Test/ScriptGenerator")]
+    public static void Window()
+    {
+        GetWindow<ScriptGeneratorWindow>("ScriptGenerator");//windowを生成
+    }
+
+    private void OnGUI()
+    {
+        _selectTemplates = (SelectTemplates)EditorGUILayout.EnumPopup("SelectTemplates", _selectTemplates);
+        GUILayout.Label("Script Name", EditorStyles.boldLabel);//太字で表示
+        _scriptName = EditorGUILayout.TextField(_scriptName);
+
+        if (GUILayout.Button("Generate Script"))
+        {
+            GenerateScript();
+        }
+    }
+
+    private void GenerateScript()
+    {
+        string template = "";
+
+        switch (_selectTemplates)
+        {
+            case SelectTemplates.MonoBehaviour:
+                template = _monoBehaviourTemplate;
+                break;
+            case SelectTemplates.Class:
+                template = _classTemplate;
+                break;
+            case SelectTemplates.Interface:
+                template = _interfaceTemplate;
+                break;
+        }
+
+        string scriptContent = template.Replace("#SCRIPTNAME#", _scriptName);
+        //パスを指定する
+        string filePath = EditorUtility.SaveFilePanel("Save", "Assets", _scriptName, "cs");
+
+        if (!string.IsNullOrEmpty(filePath))
+        {
+            //指定したパスにファイルを作成してテキストを書き込む
+            System.IO.File.WriteAllText(filePath, scriptContent);
+            AssetDatabase.Refresh();//エディタ上に反映する
+        }
+    }
+}
