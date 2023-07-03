667ad1dfd6ee3a6740d77499e5239664572f2a30
RK <riku.kuu0421@gmail.com>
2023-07-03 23:55:31 +0900

    [add]Nodeクラスを作成

---
+++ Assets/Node.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public abstract class Node : ScriptableObject
+{
+
+    public enum NodeState
+    { 
+        Running,
+        Failure,
+        Success
+    }
+
+    private NodeState _state = NodeState.Running;
+
+    public NodeState State => _state;
+
+    private bool _isStart = false;
+
+    public NodeState Update()
+    {
+        if(!_isStart)
+        {
+            OnStart();
+            _isStart = true;
+        }
+
+        _state = OnUpdate();
+
+        if(_state == NodeState.Failure || _state == NodeState.Success)
+        {
+            OnStop();
+            _isStart = false;
+        }
+
+        return _state;
+    }
+
+    protected abstract void OnStart();
+    protected abstract void OnStop();
+    protected abstract NodeState OnUpdate();
+
+}
